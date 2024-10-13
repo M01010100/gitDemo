b64b2be1f5c8cc478b23d223b128b8e67ff5f50b
m01010100 <townsemc@sunypoly.edu>
2024-10-12 20:52:22 -0400

    Inital Commit of hello.cpp/.go

---
+++ hello.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string print_following(string word_1, string word_2, int n){
+	string out;
+	for(int i = 0; i < n; i++){
+		out.append(word_1 + ' ');
+		out.append(word_2 + '\n');
+	}
+	return out;
+}
+
+int main(){
+	string one = "Pesto is";
+	string two = "a Penguin";
+
+	string display = print_following(one, two, 2);
+	cout << display;
+	return 0;
+}
---
+++ hello.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+var outpu string
+
+func printOut(in_1 string, in_2 string, n int) string {
+	var sb strings.Builder
+	for i := 0; i < n; i++ {
+		sb.WriteString(in_1)
+		sb.WriteString(" ")
+		sb.WriteString(in_2)
+		sb.WriteString("\n")
+	}
+	return sb.String()
+}
+func main() {
+	output := printOut("Pesto is", "a Penguin", 2)
+	fmt.Println(output)
+}
